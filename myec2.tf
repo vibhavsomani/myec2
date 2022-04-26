8dcea29189539f1493b56ba9e24131b0dd4f35d8
vibhavsomani <40886570+vibhavsomani@users.noreply.github.com>
2022-04-26 15:48:58 +0530

    Update myec2.tf

--- myec2.tf
+++ myec2.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
 resource "aws_instance" "myec2" {
     ami = "ami-00ee4df451840fa9d"
     instance_type = "t2.micro"   
